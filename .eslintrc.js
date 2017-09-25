402ad8cbf3c784fb210c5e5c46c8bea16beceb6a
Matthew Wood <mwood@financialforce.com>
2017-09-25 10:04:25 +0100

    Use eslint on the lint file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 FinancialForce.com, inc.  All rights reserved.
+ *
+ * This is a proof of concept.
+ * It forms the basis of recommendations for architecture, patterns, dependencies, and has
+ * some of the characteristics which will eventually be present in FF hybrid apps.
+ *
+ * However, it lacks important functionality and checks.
+ * It does not conform to SOC1 or any other auditable process.
+ *
+ * We do not endorse any attempt to use this codebase as the blueprint for production code.
+ */
+'use strict';
+
+module.exports = {
+	"extends": "./index.js"
+}
