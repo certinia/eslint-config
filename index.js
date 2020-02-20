6ecfd79fc726ad254ed8977a01a8af2998646c00
Matthew Wood <mwood@financialforce.com>
2020-02-20 16:08:19 +0000

    Remove the typescript override extends

--- index.js
+++ index.js
@@ -599,10 +599,6 @@ module.exports = {
 		},
 		{
 			files: ['**/*.ts'],
-			['extends']: [
-				'plugin:@typescript-eslint/recommended',
-				'plugin:@typescript-eslint/recommended-requiring-type-checking'
-			],
 			parser: '@typescript-eslint/parser',
 			parserOptions: {
 				project: 'tsconfig.json'
